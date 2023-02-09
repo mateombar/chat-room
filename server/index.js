95edc1eb51be49a4363bdc0a2a386c0972629ed4
Mateo Montanez Baron <mateomontabaron@gmail.com>
2023-02-09 07:02:04 -0500

    Add connection, message and close ws events

--- server/index.js
+++ server/index.js
@@ -1,9 +1,52 @@
-const WebSocket = require('ws');
+const WebSocket = require("ws");
+const users = new Set();
 
-const server = new WebSocket.Server({
-        port: 8080
-    },
-    () => {
-        console.log('Server started on port 8080');
-    }
+function sendMessage(message) {
+  users.forEach((user) => {
+    user.ws.send(JSON.stringify(message));
+  });
+}
+
+const server = new WebSocket.Server(
+  {
+    port: 8080,
+  },
+  () => {
+    console.log("Server started on port 8080");
+  }
 );
+
+server.on("connection", (ws) => {
+  const userRef = { ws };
+  users.add(userRef);
+
+  ws.on("message", (message) => {
+    console.log(message);
+    try {
+      // Parsing the message
+      const data = JSON.parse(message);
+
+      // Check if the message is valid
+      if (typeof data.sender !== "string" || typeof data.body !== "string") {
+        console.error("Invalid message");
+        return;
+      }
+
+      // Sending the message
+      const messageToSend = {
+        sender: data.sender,
+        body: data.body,
+        sentAt: Date.now(),
+      };
+
+      sendMessage(messageToSend);
+    } catch (e) {
+      console.error("Error passing message!", e);
+    }
+  });
+
+  ws.on('close', (code, reason) => {
+    users.delete(userRef);
+    console.log(`Connection closed: ${code} ${reason}!`);
+  })
+});
