ce46c6474a426180c0271e1b216accf5ab762627
Mateo Montanez Baron <mateomontabaron@gmail.com>
2023-02-09 07:12:36 -0500

    Add login page

---
+++ src/components/Layout.js
@@ -0,0 +1,10 @@
+function Layout({ children }) {
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center space-y-6">
+      <h2 className="text-3xl font-bold">React Ws Chat</h2>
+      {children}
+    </div>
+  );
+}
+
+export default Layout;
